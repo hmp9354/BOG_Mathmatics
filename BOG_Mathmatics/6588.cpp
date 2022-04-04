5411117e5d49adf32084d064c7e37b55f8eec7e0
nirip <phm0259@gmail.com>
2022-04-04 15:58:38 +0900

    22/04/04

---
+++ BOG_Mathmatics/6588.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <math.h>
+
+void PN(int num, char list[]) {	
+
+	int tmp = sqrt(num);
+
+	for (int i = 2; i <= tmp; i++) {
+		if (list[i]) { continue; }
+		for (int j = i + i; j <= num; j += i) {
+			list[j] = 1;
+		}
+	}	
+}
+
+int main() {
+	int num;
+	char list[1000002] = { 0 };
+	int a, b;
+	PN(1000002, list);
+
+	while (1) {
+		scanf_s("%d", &num);
+		if (!num) { break; }
+
+		bool result = false;
+		
+		for (int i = 2; i <= num; i++) {
+			if (list[i]) { continue; }
+
+			int sPN = num - i;
+			a = i;
+			b = 0;
+			if (!list[sPN]) {
+				b = sPN;
+				result = true;
+				break;
+			}
+		}
+
+		if (result) {
+			printf("%d = %d + %d\n", num, a, b);
+		}
+		else {
+			printf("Goldbach's conjecture is wrong.");
+		}
+	}
+}
